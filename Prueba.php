f37c40eb8a3561b92707496be5d92669b6ea5266
Stefany2 <1459565@senati.pe>
2023-04-26 18:02:49 -0500

    My first Hello world with PHP

---
+++ Prueba.php
@@ -0,0 +1,15 @@
+<!--Hola mundo en el servidor y php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My First Web</title>
+</head>
+<body>
+    <!--Mensaje-->
+    <h1>Hello world, desde el servidor</h1>
+    <h2><?php echo "Hello wold in PHP" ?></h2>
+</body>
+</html>
